1dd22104ef39b6095668aad1dfa32d59f5fecfbf
Jason Boyd <jasonboyd99@gmail.com>
2025-01-21 20:48:40 -0700

    Added testing files and started them for templates and utilities.

---
+++ tests/test_templates.py
@@ -0,0 +1,26 @@
+import pathlib
+import datetime
+from templates import templates as tp
+
+
+class TestTemplates:
+    def test_create_directory_table(self):
+        pass
+
+    def test_generate_directory_information(self):
+        pass
+
+    def test_build_table_string(self):
+        pass
+
+    def test_get_file_path_list(self):
+        pass
+
+    def test_handle_single_stat_file(self):
+        pass
+
+    def test_handle_list_stat_file(self):
+        pass
+
+    def test_convert_seconds_iso(self):
+        pass
---
+++ tests/test_utilities.py
@@ -0,0 +1,16 @@
+import click
+from utilities import utilities as ut
+
+
+class TestUtilities:
+    def test_directory_list(self):
+        pass
+
+    def test_handle_directory_list(self):
+        pass
+
+    def test_copy_template(self):
+        pass
+
+    def test_check_template_configuration(self):
+        pass
