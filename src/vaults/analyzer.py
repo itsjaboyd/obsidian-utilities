08e2be4960f3550c362fcad0b79f72edf7257ec6
Jason Boyd <jasonboyd99@gmail.com>
2025-02-02 18:37:39 -0700

    Added vaults pakcage and startings of an Analyzer class.

---
+++ src/vaults/analyzer.py
@@ -0,0 +1,26 @@
+"""_summary_"""
+
+
+class Analyzer:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def calculate_copied_paths(self, template, target, copies=1, formatting=True):
+        pass
+
+    def calculate_multiple_copied_paths(
+        self, template, target, copies=1, formatting=True
+    ):
+        pass
+
+    def calculate_single_copied_paths(self, template, target, formatting=True):
+        pass
+
+    def gather_directories(self, directory):
+        pass
+
+    def gather_files(self, directory, suffix=None):
+        pass
